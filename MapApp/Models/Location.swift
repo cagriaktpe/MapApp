1787066bc6d05555130c874e47b3a8ea34aace22
Samet Çağrı Aktepe <sametcagriaktepe@gmail.com>
2024-02-19 15:12:20 +0300

    Import assets and set up initial settings

---
+++ MapApp/Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  MapApp
+//
+//  Created by Samet Çağrı Aktepe on 19.02.2024.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
