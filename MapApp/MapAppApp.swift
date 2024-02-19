435680de1c07b29f5514d7cd1a865d76008cbd74
Samet Çağrı Aktepe <sametcagriaktepe@gmail.com>
2024-02-19 14:25:30 +0300

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Samet Çağrı Aktepe on 19.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
